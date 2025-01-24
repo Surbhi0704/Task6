86637dcfdef4875fc430ba98b13295f672473a68
Surbhi0704 <surbhiviradiya07@gmail.com>
2025-01-24 12:25:50 +0530

    Initial commit for Task 6 - Node.js project

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const projectDir = __dirname;
+
+console.log(chalk.red(projectDir));
